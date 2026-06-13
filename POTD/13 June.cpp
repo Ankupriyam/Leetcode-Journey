67928d997856396eea5add0fcd8b92e18c8deb4a
ankupriyam <anku.priyam@gmail.com>
2026-06-13 06:37:30 +0530

    13 June solution

---
+++ POTD/13 June.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans="";
+        for(int i=0;i<words.size();i++){
+            long long sum=0;
+            for(int j=0;j<words[i].size();j++){
+                sum+=weights[words[i][j]-'a'];
+            }
+            sum=sum%26;
+            ans.push_back(char('z'-sum));
+        }
+        return ans;
+    }
+};
