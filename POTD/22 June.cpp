cec45166a23eb8c819a946131beede78f6133dd7
ankupriyam <anku.priyam@gmail.com>
2026-06-22 06:16:16 +0530

    22 June

---
+++ POTD/22 June.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int a = 0, b = 0, l = 0, o = 0, n = 0;
+        for (char c : text) {
+            if (c == 'a') {
+                a++;
+            } else if (c == 'b') {
+                b++;
+            } else if (c == 'l') {
+                l++;
+            } else if (c == 'o') {
+                o++;
+            } else if (c == 'n') {
+                n++;
+            }
+        }
+        return min({a, b, l / 2, o / 2, n});
+    }
+};
