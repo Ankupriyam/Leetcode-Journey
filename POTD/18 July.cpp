b545186427ad0c7fd075842c2b36b0570b264b3f
ankupriyam <anku.priyam@gmail.com>
2026-07-18 06:06:51 +0530

    17 july potd

---
+++ POTD/18 July.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int mini=1e9,maxi=0;
+        for(int i=0;i<nums.size();i++){
+            mini=min(mini,nums[i]);
+            maxi=max(maxi,nums[i]);
+        }
+        return gcd(mini,maxi);
+    }
+};
